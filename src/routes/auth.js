31c686975b2b590418c8e36d0a529f9ced2a707c
jocim-nix <joe.susatiyo@gmail.com>
2026-02-28 15:48:34 +0700

    feat: added logout route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { register, login, me } from "../controllers/authController.js";
+import { register, login, logout, me } from "../controllers/authController.js";
 import { authRequired } from "../middleware/auth.js";
 
 export const authRouter = Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
+authRouter.post("/logout", logout);
 authRouter.get("/me", authRequired, me);
