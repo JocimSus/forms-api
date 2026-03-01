4e03170e27285fe9d8ea0b66a3d6f319240afa7e
jocim-nix <joe.susatiyo@gmail.com>
2026-03-01 13:39:57 +0700

    fix: corsConfig now uses .env

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,9 +1,7 @@
 import cors from "cors";
 
-// export const corsConfig = cors({
-//   origin: "https://forms.224668.xyz",
-//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-//   credentials: true,
-// });
-
-export const corsConfig = cors();
+export const corsConfig = cors({
+  origin: process.env.CORS_ORIGIN,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: true,
+});
